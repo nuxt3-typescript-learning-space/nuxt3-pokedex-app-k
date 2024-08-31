a304955f7a04f1e6c4a9067ae12274faddfcd09f
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-08-31 09:32:27 +0900

    :sparkles: feat: ポケモンの詳細ページ

---
+++ src/features/pokedex/name/components/DetailPage.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import DetailCard from './DetailCard.vue';
+
+const pokemonSpecificDataStore = usePokemonSpecificDataStore();
+const { updateSpecificPokemonData } = pokemonSpecificDataStore;
+
+const route = useRoute();
+const router = useRouter();
+
+const nameId = route.query.nameId as string;
+
+const handleClickBackButton = () => {
+  router.back();
+};
+
+await updateSpecificPokemonData(nameId);
+</script>
+<template>
+  <DetailCard />
+  <div class="flex justify-center">
+    <Button class="mt-4 bg-green-400 font-bold hover:bg-green-300" @click="handleClickBackButton">もどる</Button>
+  </div>
+</template>
