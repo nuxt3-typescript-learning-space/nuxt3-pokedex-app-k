8ff0e3429a7de4e4adceb69450a53e5114e490fe
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-08-31 09:09:08 +0900

    :lipstick: style: 画像の汎用コンポーネント追加

---
+++ src/components/layout/PokemonImage.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+type Props = {
+  src?: string;
+  alt?: string;
+};
+
+defineProps<Props>();
+</script>
+<template>
+  <template v-if="src">
+    <img :src="src" :alt="alt" />
+  </template>
+  <template v-else>
+    <img src="@/assets/image/no-image.png" alt="画像なし" />
+  </template>
+</template>
